f288cdf6c6c09c76913b7b6a75ed3c67f7d3b56c
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-08-01 20:36:14 -0400

    [esolang] Added basic interpreter.
    Made a modular interpreter for lang.
    
    Most interesting operations/logic is implemented, but is subject to change.

---
+++ challenges/programming/esolang/ctf/enums.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class Tile(Enum):
+    EMPTY = ord(' ')
+    PLAYER = ord('@')
+    PRINT = ord('.')
+    LEFT = ord('<')
+    RIGHT = ord('>')
+    UP = ord('^')
+    DOWN = ord('v')
+    EQUAL = ord('=')
+    POP = ord('!')
+    COPY = ord('$')
+    FLAG = ord('#')
+    DROP = ord('_')
+    SPACE = ord('?')
+    ADD = ord('+')
+    SUBSTRACT = ord('-')
+    INPUT = ord(',')
+    # ZERO = ord('?')
+
+class Direction(Enum):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
