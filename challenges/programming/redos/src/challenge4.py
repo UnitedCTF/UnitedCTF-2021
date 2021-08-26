843e583159e58ffc57af44ff4c34bb6fb8c6dc4b
Olivier Langlois <olivier.langlois@polyrix.com>
2021-08-26 09:53:47 -0400

    removed debug statement in chall4

--- challenges/programming/redos/src/challenge4.py
+++ challenges/programming/redos/src/challenge4.py
@@ -1,13 +1,10 @@
 import re
 
-# msg = input()
-msg = 'f \tffffffffffffffffffff_DaddyIssues'
+msg = input()
 a = sum(map(ord, msg)) % 35
 b = len(msg.split('_')[0])
 c = len(msg)
 
-print(a, b, c)
-
 reg_str = f'^((?P<a5>\\S)|(?P<a14>(?<!(?(a5)[\\w\\W]|.)))|(?P<a23> )|(?P<a35>\\s)){{{c % 16}}}(?(a{c})(?(a{b})(?P=a{a})+|(?(a14)\\w+|(?P=a23)))|_)+$'
 regex = re.compile(reg_str)
-print(regex.match(msg))
+regex.match(msg)
