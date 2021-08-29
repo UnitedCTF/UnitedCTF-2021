85a13943884d8c483cb95f58d9a1f2f73edb8255
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2021-08-29 02:38:23 -0400

    Cookies and Timing attack page templates

---
+++ challenges/web/cookies/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Web Connect™ Login</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="styles.css">
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+	</head>
+
+	<body>
+		<div class="main">
+			<p class="sign" align="center">Sign in</p>
+			<form class="form1">
+				<input class="un" type="text" align="center" placeholder="Username">
+				<input class="pass" type="password" align="center" placeholder="Password">
+				<a class="submit" align="center">Sign in</a>
+				<p class="forgot" align="center"><a href="#">Forgot Password?</p>
+			</form>
+		</div>
+	</body>
+</html>
