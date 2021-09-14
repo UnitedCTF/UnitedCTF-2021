a783eccf77a8785401ebf7f855c5cec937d1151c
StrixSC <talon.strix@gmail.com>
2021-09-13 21:55:49 -0400

    fixed docker not starting server on the right port

--- challenges/reverse/character_customization/private/server.py
+++ challenges/reverse/character_customization/private/server.py
@@ -4,14 +4,14 @@ import subprocess
 
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-listener.bind('0.0.0.0', 9001)
+listener.bind((os.environ.get("URL"), int(os.environ.get("SERVER_PORT"))))
 listener.listen(5)
 print(listener.getsockname())
 
 try:
     while True:
         client, addr = listener.accept()
-        subprocess.Popen(['./gamepad'], stdin=client, stdout=client, stderr=client)
+        subprocess.Popen(['./' + os.environ.get("PROGRAM_NAME")], stdin=client, stdout=client, stderr=client)
         client.close()
 except KeyboardInterrupt:
     pass
