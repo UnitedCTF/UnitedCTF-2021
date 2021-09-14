6d7e099562d454e87bb3f300b550586f8bf8df6b
Vincent Laferrière <github@monordibogue.com>
2021-09-14 10:23:20 -0400

    Crypto paddingoracle (#12)
    * added challenge
    
    * removed timeout
    
    * fixed build
    
    * aded .gitignore
    
    * added readme
    
    * changed port

---
+++ challenges/crypto/paddingoracle/README.md
@@ -0,0 +1,21 @@
+# Padding oracle
+> Crypto
+Author: @KptCheeseWhiz
+
+## Challenge
+
+Can you generate a ciphertext with the plaintext FLAG in it?
+ - the whole source code is given
+
+## Resources
+
+ - https://en.wikipedia.org/wiki/Padding_oracle_attack
+
+## Setup
+
+### Requirements
+  - docker
+  - docker-compose
+
+### How to run
+  - Execute the command `docker-compose up`
---
+++ challenges/crypto/paddingoracle/src/index.ts
@@ -0,0 +1,44 @@
+import express from "express";
+
+import { decrypt, encrypt } from "./crypto";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", require("path").join(__dirname, "views"));
+
+app.get("/decrypt", (req: express.Request, res: express.Response) => {
+  const ciphertext = req.query.ciphertext;
+  if (typeof ciphertext !== "string")
+    return res.status(400).send("invalid request");
+  const plaintext = decrypt(ciphertext);
+  if (plaintext.indexOf("FLAG") !== -1) return res.send(process.env.FLAG);
+  res.send(plaintext);
+});
+
+app.get("/encrypt", (req: express.Request, res: express.Response) => {
+  const plaintext = req.query.plaintext;
+  if (typeof plaintext !== "string")
+    return res.status(400).send("invalid request");
+  if (plaintext.indexOf("FLAG") !== -1)
+    return res.status(403).send("forbidden word detected");
+  res.send(encrypt(plaintext));
+});
+
+app.get("/", (_: express.Request, res: express.Response) =>
+  res.render("index")
+);
+
+app.use(function (
+  err: Error,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  res.status(400).send(err.message);
+});
+
+app.listen(Number(process.env.PORT || 5000));
